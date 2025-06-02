1ad9763e2f8c064b30247b1b8211098c9eb180c3
Koaladeer <s0587617@htw-berlin.de>
2025-06-02 16:26:07 +0200

    humble beginnings

---
+++ game/environment.py
@@ -0,0 +1,19 @@
+import json
+
+class GameEnvironment:
+    def __init__(self, config_file="data/world_config.json"):
+        with open(config_file) as f:
+            self.world = json.load(f)
+        self.current_room = self.world["start"]
+
+    def get_state(self):
+        return self.world["rooms"][self.current_room]["description"]
+
+    def perform_action(self, action):
+        valid = self.world["rooms"][self.current_room].get("actions", {})
+        if action in valid:
+            self.current_room = valid[action]
+            return True
+        return False
+    def is_terminal(self):
+        return self.world["rooms"][self.current_room].get("terminal", False)
